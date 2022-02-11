2d8b0026a64f9ae437fe8efd7864237d18f1ff9a
Abhishek Tirkey <abhishek.tirkey@verifiably.com>
2022-02-11 13:29:08 +0530

    Complete Product Schema and add Category Schema

--- Backend/models/Products.js
+++ Backend/models/Products.js
@@ -1,11 +1,56 @@
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-  name: String,
-  image: String,
+  name: {
+    required: true,
+    type: String,
+  },
+  description: {
+    required: true,
+    type: String,
+  },
+  richDescription: {
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  brand: {
+    type: String,
+    default: "",
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
   countInStock: {
     type: Number,
     required: true,
+    min: 0,
+    max: 300,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
   },
 });
 
