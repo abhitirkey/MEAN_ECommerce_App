0e654243f1559488815e725b90fdf8eaf22e535a
Abhishek Tirkey <abhishek.tirkey@verifiably.com>
2022-02-12 23:53:53 +0530

    Add first 2 components to frontend

--- Frontend/abespace/apps/admin/src/app/app.module.ts
+++ Frontend/abespace/apps/admin/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { HomepageComponent } from './pages/homepage/homepage.component';
-import { ProjectListComponent } from './pages/project-list/project-list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, HomepageComponent, ProjectListComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
